a275e6082f2d33ded268619c15b67f7db825b689
abir_zishan <118625188+abirzishan32@users.noreply.github.com>
2024-05-07 15:55:31 +0600

    Update hospital_management_system.sql

--- hospital_management_system.sql
+++ hospital_management_system.sql
@@ -76,36 +76,5 @@ insert into appointment values (400, '2024-12-12', '3.00pm', 18, 1);
 insert into appointment values (401, '2024-11-11', '4.00pm', 28, 2);
 insert into appointment values (402, '2024-12-11', '3.00pm', 38, 3);
 
-CREATE OR REPLACE VIEW appointment_details_view AS
-SELECT 
-    a.app_si,
-    a.app_date,
-    a.app_time,
-    p.p_id,
-    p.p_name,
-    p.gender,
-    p.age,
-    p.weight,
-    ph.doc_code,
-    ph.doc_name,
-    ph.designation,
-    d.dept_id,
-    d.dept_name,
-    d.total_doc,
-    d.dept_head,
-    hb.branch_no,
-    hb.location,
-    hb.contact_no AS branch_contact,
-    hb.email AS branch_email
-FROM 
-    appointment a
-JOIN 
-    patient p ON a.p_id = p.p_id
-JOIN 
-    physician ph ON a.doc_code = ph.doc_code
-JOIN 
-    department d ON ph.dept_id = d.dept_id
-JOIN 
-    hospital_branch hb ON d.branch_no = hb.branch_no;
 
 /
